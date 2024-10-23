b69d0b2a7672ef2d3576f9833b72a282e2153b2d
Glenlg22 <glenlagrange22@gmail.com>
2024-10-23 15:03:36 +0200

    RestClients added

---
+++ J148LibraryFrontend/src/main/java/com/j148/j148libraryfrontend/restclients/ReservationRestClient.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.j148.j148libraryfrontend.restclients;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.j148.j148libraryfrontend.models.User;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author glenl
+ */
+public class ReservationRestClient {
+
+    private WebTarget webTarget;
+    private Client client;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private String uri = "http://localhost:8080/J148LibraryBackend/reservation/";
+    private static final Logger LOG = Logger.getLogger(UserRestClient.class.getName());
+
+    private String writeObjectAsJson(Object object) throws JsonProcessingException {
+        return this.objectMapper.writeValueAsString(object);
+    }
+}
