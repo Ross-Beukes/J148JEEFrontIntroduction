2ba3c6f1b85e691179c88f41e5d583f36d076dea
Glenlg22 <glenlagrange22@gmail.com>
2024-10-23 14:41:22 +0200

    Models for front end, Book, BookBorrowed, Fines and Reservations

---
+++ J148LibraryFrontend/src/main/java/com/j148/j148libraryfrontend/models/Reservation.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.j148.j148libraryfrontend.models;
+
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author glenl
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+
+public class Reservation {
+    private long reservationId;
+    private Book book;
+    private User user;
+    private Timestamp collectionDate; 
+}
